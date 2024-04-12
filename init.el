2784a60bbb025985b837150fe563d455646c3af7
mowind <wjinwen.1988@gmail.com>
2024-04-12 12:07:04 +0800

    add dired

--- init.el
+++ init.el
@@ -61,6 +61,7 @@
 (require 'init-ui)
 (require 'init-completion)
 (require 'init-edit)
+(require 'init-dired)
 (require 'init-yasnippet)
 
 (require 'init-highlight)
@@ -79,3 +80,16 @@
 (provide 'init)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("0f76f9e0af168197f4798aba5c5ef18e07c926f4e7676b95f2a13771355ce850" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
