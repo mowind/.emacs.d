d08fe62685faf7fbb947987250bcf3e823f81e1a
mowind <wjinwen.1988@gmail.com>
2025-02-14 11:01:17 +0800

    add gptel-aibo

--- lisp/init-gptel.el
+++ lisp/init-gptel.el
@@ -10,15 +10,16 @@
               :host github
               :repo "karthink/gptel")
   :config
-  (setq gptel-default-mode 'org-mode)
-  (setq gptel-model   'ep-20250213152151-qtssm
+  ;;(setq gptel-default-mode 'org-mode)
+  (setq gptel-model  'ep-20250214105538-4ht6q ;; 'ep-20250213152151-qtssm
         gptel-backend
         (gptel-make-openai "Volces-DeepSeek"     ;Any name you want
-          :host "ark.cn-beijing.volces.com/api/v3"
-          :endpoint "/chat/completions"
-          :stream t
-          :key 'gptel-api-key             ;can be a function that returns the key
-          :models '(ep-20250213152151-qtssm))))
+                           :host "ark.cn-beijing.volces.com/api/v3"
+                           :endpoint "/chat/completions"
+                           :stream t
+                           :key 'gptel-api-key             ;can be a function that returns the key
+                           :models '(ep-20250214105538-4ht6q))))
+;;:models '(ep-20250213152151-qtssm))))
 ;;(setq gptel-model   'deepseek-chat
 ;;      gptel-backend
 ;;      (gptel-make-openai "DeepSeek"     ;Any name you want
@@ -28,10 +29,19 @@
 ;;        :key 'gptel-api-key             ;can be a function that returns the key
 ;;        :models '(deepseek-chat))))
 
-(defun gptel+ ()
-  (interactive)
-  (let ((locale-coding-system 'utf-8))
-    (call-interactively #'gptel)))
+(use-package gptel-aibo
+  :after (gptel)
+  :straight '(gptel-aibo
+              :type git
+              :host github
+              :repo "dolmens/gptel-aibo")
+  :config
+  ;;(define-key gptel-aibo-mode-map
+  ;;  (kbd "C-c /") #'gptel-aibo-apply-last-suggestions)
+
+  ;;  (define-key gptel-aibo-complete-mode-map
+  ;;  (kbd "C-c i") #'gptel-aibo-complete-at-point)
+  (add-hook 'prog-mode-hook #'gptel-aibo-complete-mode))
 
 (provide 'init-gptel)
 ;;; init-gptel.el ends here
