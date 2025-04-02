98eb53ab362889f51c45f669cbd718e4278a08ac
mowind <wjinwen.1988@gmail.com>
2025-04-02 14:34:32 +0800

    add openrouter

--- lisp/init-gptel.el
+++ lisp/init-gptel.el
@@ -11,23 +11,36 @@
               :repo "karthink/gptel")
   :config
   ;;(setq gptel-default-mode 'org-mode)
-  (setq gptel-model  'ep-20250214105538-4ht6q ;; 'ep-20250213152151-qtssm
-        gptel-backend
-        (gptel-make-openai "Volces-DeepSeek"     ;Any name you want
-                           :host "ark.cn-beijing.volces.com/api/v3"
-                           :endpoint "/chat/completions"
-                           :stream t
-                           :key 'gptel-api-key             ;can be a function that returns the key
-                           :models '(ep-20250214105538-4ht6q))))
-;;:models '(ep-20250213152151-qtssm))))
-;;(setq gptel-model   'deepseek-chat
-;;      gptel-backend
-;;      (gptel-make-openai "DeepSeek"     ;Any name you want
-;;        :host "api.deepseek.com"
-;;        :endpoint "/chat/completions"
-;;        :stream t
-;;        :key 'gptel-api-key             ;can be a function that returns the key
-;;        :models '(deepseek-chat))))
+  (gptel-make-deepseek "DeepSeek"
+    :stream t
+    :key #'gptel-api-key)
+  (gptel-make-openai "Volces-DeepSeek"
+    :host "ark.cn-beijing.volces.com/api/v3"
+    :endpoint "/chat/completions"
+    :stream t
+    :key #'gptel-api-key             ;can be a function that returns the key
+    :models '(ep-20250214105538-4ht6q))
+  (gptel-make-openai "OpenRouter"
+    :host "openrouter.ai"
+    :endpoint "/api/v1/chat/completions"
+    :key #'gptel-api-key
+    :models '(deepseek/deepseek-chat
+              deepseek/deepseek-r1
+              deepseek/deepseek-chat:free
+              deepseek/deepseek-r1:free
+              qwen/qwen-turbo
+              qwen/qwen-plus
+              qwen/qwen-max
+              openai/chatgpt-4o-latest
+              openai/o1
+              openai/o3-mini-high
+              anthropic/claude-3.7-sonnect:thinking
+              anthropic/claude-3.7-sonnect
+              anthropic/claude-3-opus
+              google/gemini-2.0-pro-exp-02-05:free
+              google/gemini-2.0-flash-thinking-exp:free
+              google/gemini-2.0-flash-001)
+    :stream t))
 
 (use-package gptel-aibo
   :after (gptel)
